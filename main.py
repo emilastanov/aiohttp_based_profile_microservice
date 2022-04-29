7bbf1228159d47a7351540a5caf3e79b9a9a9be2
emilastanov <emila1998@me.com>
2022-04-29 15:01:54 +0300

    Bug fixed

--- main.py
+++ main.py
@@ -41,6 +41,6 @@ app = web.Application()
 if __name__ == '__main__':
     setup_app(app)
     try:
-        web.run_app(app, port=config['common']['port'], host='localhost')
+        web.run_app(app, port=config['common']['port'], host='0.0.0.0')
     except OSError:
         print('База данных не подключена! В файле /config/config.yaml необходимо указать адресс базы данных.')
